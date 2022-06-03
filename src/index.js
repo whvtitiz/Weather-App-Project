534f1898f14580b8266a0cad16a0b38da4a991f2
whvtitiz <whitttwiles@gmail.com>
2022-06-02 20:15:41 -0700

    Added JS forecast by injecting HTML

--- src/index.js
+++ src/index.js
@@ -20,6 +20,37 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+              <div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                <img src="http://openweathermap.org/img/wn/50n@2x.png"
+                 alt="" 
+                 width="42"
+                 />
+                <div class="weather-forecast-temperatures">
+                <span class= "weather-forecast-temperature-max"> 18° </span>
+                <span class= "weather-forecast-temperature-min">12°</span>
+                </div>
+              </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 function showTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -96,3 +127,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 searchCity("New York");
+displayForecast();
